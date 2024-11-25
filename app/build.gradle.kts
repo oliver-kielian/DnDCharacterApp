c340799c671fe75a59a920ee3bc065273f5da6ea
Oliver Kielian <oliverkielian02@gmail.com>
2024-11-25 11:25:26 -0700

    changed complie sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.dndcharacterapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.dndcharacterapp"
