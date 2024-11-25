8baf2eae2242c0292fe45de8fe6d9bf0e25c9e35
Oliver Kielian <oliverkielian02@gmail.com>
2024-11-25 09:25:42 -0700

    initialized layout items

--- app/src/main/java/com/example/dndcharacterapp/MainActivity.kt
+++ app/src/main/java/com/example/dndcharacterapp/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.dndcharacterapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainToolbar : Toolbar
+    private lateinit var characterList : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        mainToolbar = findViewById(R.id.mainToolbar)
+        characterList = findViewById(R.id.characterRecyclerView)
+
     }
 }
